e0a03be3e0965b7b13427743e9f8f9a83c7801c7
chaericodes <chaericodes@users.noreply.github.com>
2023-06-12 18:36:55 +0800

    Showing the current temperature
    1. Placed the search bar inside the content div
    2. Created ids and classes for some JavaScript action
    3. Modified CSS to my liking

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 function showTemperature(response) {
-    console.log(response.data);
+    let currentTemp = document.querySelector("#main-temperature");
+    let feelsLike = document.querySelector("#feels-like-desc");
+    let description = document.querySelector(".description");
+    let currentCity = document.querySelector("#main-city");
+    let humidity = document.querySelector("#humidity");
+    let icon = document.querySelector("#main-icon");
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = Math.round(response.data.wind.speed);
+    humidity.innerHTML = Math.round(response.data.temperature.humidity);
+    currentCity.innerHTML = response.data.city;
+    currentTemp.innerHTML = Math.round(response.data.temperature.current);
+    description.innerHTML = response.data.condition.description;
+    feelsLike.innerHTML = `Feels like ${Math.round(response.data.temperature.feels_like)} °C`;
+
+
 }
 
 let apiKey = `9b041cb0e74745939f1d6ae898107fot`;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Manila&key=${apiKey}`;
-
-
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Manila&key=${apiKey}&units=metric`
 axios.get(apiUrl).then(showTemperature);
-
-let city = document.querySelector("#main-city");
-city.innerHTML = `Manila`;
-
-
